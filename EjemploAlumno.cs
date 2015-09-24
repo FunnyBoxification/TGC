727fa0f7514acb17311a119c9bfd0cf8e52e320d
FunnyBoxification <pablorom93@gmail.com>
2015-09-24 01:04:10 -0300

    Commiteo archivos

---
+++ EjemploAlumno.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TgcViewer.Example;
+using TgcViewer;
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using Microsoft.DirectX;
+using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.Input;
+using Microsoft.DirectX.DirectInput;
+using TgcViewer.Utils.Terrain;
+
+namespace AlumnoEjemplos.Quicksort
+{
+    public class EjemploAlumno : TgcExample
+    {
+
+        const float VELOCIDAD_MOVIMIENTO = 200f;
+        const float VELOCIDAD_ROTACION = 120f;
+
+        TgcScene escena;
+        TgcMesh mainMesh; //Proximamente barco principal
+        TgcSkyBox skyBox;
+
+        public override string getCategory()
+        {
+            return "AlumnoEjemplos";
+        }
+
+        public override string getName()
+        {
+            return "Grupo QuickSort";
+        }
+
+        public override string getDescription()
+        {
+            return "MiIdea - Descripcion de la idea";
+        }
+
+        public override void init()
+        {
+            //GuiController.Instance: acceso principal a todas las herramientas del Framework
+
+            //Device de DirectX para crear primitivas
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
+
+            //Cargo la escena completa que tendria que ser la del escenario con el cielo / la del agua
+            //PROXIMAMENTE, ahora cargo otro escenario
+            
+            TgcSceneLoader loader = new TgcSceneLoader();
+            escena = loader.loadSceneFromFile(GuiController.Instance.ExamplesMediaDir + "MeshCreator\\Scenes\\Isla\\Isla-TgcScene.xml");
+            
+            //Textura del skybox
+            string texturesPath = GuiController.Instance.ExamplesMediaDir + "Texturas\\Quake\\SkyBox LostAtSeaDay\\";
+
+            //Crear SkyBox 
+            skyBox = new TgcSkyBox();
+            skyBox.Center = new Vector3(0, 0, 0);
+            skyBox.Size = new Vector3(3000, 1000, 3000);
+
+            //Configurar color
+            //skyBox.Color = Color.OrangeRed;
+
+            //Configurar las texturas para cada una de las 6 caras
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Up, texturesPath + "lostatseaday_up.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Down, texturesPath + "lostatseaday_dn.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Left, texturesPath + "lostatseaday_lf.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Right, texturesPath + "lostatseaday_rt.jpg");
+
+            //Hay veces es necesario invertir las texturas Front y Back si se pasa de un sistema RightHanded a uno LeftHanded
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Front, texturesPath + "lostatseaday_bk.jpg");
+            skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Back, texturesPath + "lostatseaday_ft.jpg");
+
+
+
+            //Actualizar todos los valores para crear el SkyBox
+            skyBox.updateValues();
+
+            //Cargo el mesh del/los barco/s -> porque se carga como escena y no cargo el mesh directamente?
+            TgcScene scene2 = loader.loadSceneFromFile(GuiController.Instance.ExamplesMediaDir + "MeshCreator\\Meshes\\Vehiculos\\Canoa\\Canoa-TgcScene.xml");
+            mainMesh = scene2.Meshes[0];
+
+            
+            //Camara en tercera persona focuseada en el barco (canoa) 
+            //GuiController.Instance.ThirdPersonCamera.Enable = true;
+            //GuiController.Instance.ThirdPersonCamera.setCamera(mainMesh.Position, 200, 300);
+
+
+            //PARA DESARROLLO DEL ESCENARIO ES MEJOR MOVERSE CON ESTA CAMARA
+            GuiController.Instance.FpsCamera.Enable = true;
+
+            //Carpeta de archivos Media del alumno
+            //string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
+
+
+        }
+
+        public override void render(float elapsedTime)
+        {
+            //Device de DirectX para renderizar
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
+
+            /*
+            //Obtener valor de UserVar (hay que castear)
+            int valor = (int)GuiController.Instance.UserVars.getValue("variablePrueba");
+            
+
+            //Obtener valores de Modifiers
+            float valorFloat = (float)GuiController.Instance.Modifiers["valorFloat"];
+            string opcionElegida = (string)GuiController.Instance.Modifiers["valorIntervalo"];
+            Vector3 valorVertice = (Vector3)GuiController.Instance.Modifiers["valorVertice"];
+            */
+
+            //Calcular proxima posicion de personaje segun Input
+            float moveForward = 0f;
+            float rotate = 0;
+            TgcD3dInput d3dInput = GuiController.Instance.D3dInput;
+            bool moving = false;
+            bool rotating = false;
+
+            //Adelante
+            if (d3dInput.keyDown(Key.W))
+            {
+                moveForward = -VELOCIDAD_MOVIMIENTO;
+                moving = true;
+            }
+
+            //Atras
+            if (d3dInput.keyDown(Key.S))
+            {
+                moveForward = VELOCIDAD_MOVIMIENTO;
+                moving = true;
+            }
+
+            //Derecha
+            if (d3dInput.keyDown(Key.D))
+            {
+                rotate = VELOCIDAD_ROTACION;
+                rotating = true;
+            }
+
+            //Izquierda
+            if (d3dInput.keyDown(Key.A))
+            {
+                rotate = -VELOCIDAD_ROTACION;
+                rotating = true;
+            }
+
+            //Si hubo rotacion
+            if (rotating)
+            {
+                //Rotar personaje y la camara, hay que multiplicarlo por el tiempo transcurrido para no atarse a la velocidad el hardware
+                float rotAngle = Geometry.DegreeToRadian(rotate * elapsedTime);
+                mainMesh.rotateY(rotAngle);
+                GuiController.Instance.ThirdPersonCamera.rotateY(rotAngle);
+            }
+
+            //Si hubo desplazamiento
+            if (moving)
+            {
+              
+                //Aplicar movimiento hacia adelante o atras segun la orientacion actual del Mesh
+                Vector3 lastPos = mainMesh.Position;
+
+                //La velocidad de movimiento tiene que multiplicarse por el elapsedTime para hacerse independiente de la velocida de CPU
+                //Ver Unidad 2: Ciclo acoplado vs ciclo desacoplado
+                mainMesh.moveOrientedY(moveForward * elapsedTime);
+            }
+
+            //Hacer que la camara siga al personaje en su nueva posicion
+            GuiController.Instance.ThirdPersonCamera.Target = mainMesh.Position;
+
+            //Dibujar objeto principal
+            //Siempre primero hacer todos los cálculos de lógica e input y luego al final dibujar todo (ciclo update-render)
+            mainMesh.render();
+
+            //Dibujamos la escena
+            escena.renderAll();
+
+            skyBox.render();
+
+        }
+
+        /// <summary>
+        /// Método que se llama cuando termina la ejecución del ejemplo.
+        /// Hacer dispose() de todos los objetos creados.
+        /// </summary>
+        public override void close()
+        {
+            escena.disposeAll();
+            mainMesh.dispose();
+        }
+
+
+    }
+}
